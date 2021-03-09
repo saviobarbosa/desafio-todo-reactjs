68845eec76e2765d06ab971b3537ca020faf2e47
saviobarbosa <saviiobarbosa@gmail.com>
2021-03-09 13:57:11 -0300

    :tada: Primeiro desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,28 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle && newTaskTitle !== '') {
+      setTasks((tasks) => [...tasks, { 
+        id: Math.random(), 
+        title: newTaskTitle,
+        isComplete: false
+      }]);
+    } 
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskModified = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(taskModified);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
